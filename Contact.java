59cb49fb02f0ac6f13e35af6747b99dc16a65a32
CharuSinghal <36798867+CharuSinghal@users.noreply.github.com>
2019-09-03 11:54:19 +0530

    Create Contact.java

---
+++ Contact.java
@@ -0,0 +1,67 @@
+Package.com.example.hp.food_delivery; /**
+*	Created by charu on 14-11-2018.
+*/
+public class Contvegt {
+String fname,lname,dob,email,pass,rpass; int mobile;
+//fname
+public void setFname(String fname)
+{
+this.fname = fname;
+}
+public String getFname()
+{
+return this.fname;
+}
+//lname
+public void setLname(String lname)
+7 
+
+{
+this.lname = lname;
+public String getLname()
+{
+return this.lname;
+}
+//dob
+public void setDob(String dob)
+{
+this.dob = dob;
+}
+public String getDob()
+{
+return this.dob;
+}
+//Email
+public void setEmail(String email)
+{
+this.email = email;
+}
+public String getEmail()
+{
+return this.email;
+}
+public void setPass(String pass)
+{
+this.pass = pass;
+}
+public String getPass()
+{
+return this.pass;
+}
+//rpass
+public void setRpass(String pass)
+{
+this.pass = pass;
+}
+public String getRpass()
+{
+return this.rpass;
+}
+public void setMobile(int mobile)
+{
+this.mobile = mobile;
+}
+public int getMobile()
+{
+return this.mobile;
+}
